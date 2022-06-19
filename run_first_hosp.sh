8eb32ad1ff5a916699684ad89c381a7ffcae3826
Ana Trisovic <ana.trisovic@gmail.com>
2022-06-19 02:13:52 -0400

    first hosp cosmetics

--- run_first_hosp.sh
+++ run_first_hosp.sh
@@ -7,16 +7,17 @@ job_directory=$PWD/.job
 job_file="${job_directory}/job${i}.job"
 
 echo "#!/bin/bash
-#SBATCH --job-name=job${i}.job
+#SBATCH --job-name=first_hosp
 #SBATCH --output=.out/${i}first_hosp.out
 #SBATCH --error=.out/${i}first_hosp.err
 #SBATCH -c 30
 #SBATCH --time=5-00:00
-#SBATCH --mem=200GB
+#SBATCH --mem=250GB
 #SBATCH --qos=normal
 #SBATCH -p serial_requeue 
+#SBATCH --mail-type=END
+#SBATCH --mail-user=anatrisovic@g.harvard.edu
 module load python/3.8.5-fasrc01
+pip install fastparquet
 python src/get_first_hosp.py" > $job_file 
-sbatch $job_file  
-
-
+sbatch --begin=now+4hour $job_file  
--- src/get_first_hosp.py
+++ src/get_first_hosp.py
@@ -1,6 +1,8 @@
+""" Finds first diagnosis for all diagnoses of interest. """
+
+import json
 import pandas as pd
 import numpy as np
-import json
 
 
 def get_outcomes():
@@ -24,48 +26,46 @@ def is_aki_secondary(df):
 
 
 outcomes = get_outcomes()
+co_morbidity = ["diabetes", "csd", "ihd", "pneumonia", "hf", "ami", "cerd", "uti"]
 
 if __name__ == '__main__':
     li = []
     for year in range(2000, 2017):
-        filename = "data/medpar_vars/medpar_n" + str(year) + ".csv"
-        df = pd.read_csv(filename, index_col=None, header=0)
+        filename = "data/medpar_vars/medpar_" + str(year) + ".parquet"
+        df = pd.read_parquet(filename, index_col=None, header=0)
         li.append(df)
 
     admissions = pd.concat(li, axis=0, ignore_index=True)
-    
     admissions['ADATE'] = pd.to_datetime(admissions['ADATE'])
     admissions_len = len(admissions)
 
     first_hosp_dict = dict()
 
-    # aki secondary else primary
-    co_morbidity = ["diabetes", "csd", "ihd", "pneumonia", "hf", "ami", "cerd", "uti"]
+    # get first hospitalization
     for d in co_morbidity:
-        # sets True iff d_primary and aki_secondary are True
-        admissions[d + '_primary_aki_secondary'] = admissions[
-            [d + "_primary", "aki_secondary"]].all(axis=1)
-
-
-    # correct to get only first hospitalization
-    for d in co_morbidity:
-        # group by person QID, find index of min year
+        # group by person QID, find index of min admission date
         ind_list = admissions[admissions[d + "_primary_aki_secondary"]].\
             groupby('QID')['ADATE'].idxmin()
         first_hosp_dict[d + "_primary_aki_secondary_first_hosp"] = ind_list
 
+    # aki for testing
+    ind_list = admissions[admissions['aki_primary_secondary']].\
+        groupby('QID')['ADATE'].idxmin()
+    first_hosp_dict["aki_primary_secondary_first_hosp"] = ind_list
+    
     # look at diabetes and aki only
     helper_aki = admissions[admissions[
         ['aki_primary', 'aki_secondary', 'diabetes_primary', 'diabetes_secondary']].\
         any(axis=1)].groupby("QID").apply(is_aki_secondary)
     first_hosp_dict['diabeteshosp_prior_aki'] = helper_aki.dropna()
+
     # look at ckd and aki
     helper_aki = admissions[admissions[
         ['aki_primary', 'aki_secondary', 'ckd_primary', 'ckd_secondary']]. \
         any(axis=1)].groupby("QID").apply(is_aki_secondary)
     first_hosp_dict['ckdhosp_prior_aki'] = helper_aki.dropna()
 
-    # add primary and secondary so that only the first diag counts
+    # get first hosp for primary and secondary diags for outcome 
     for outcome in outcomes:
         ind_list = admissions[admissions[outcome + "_primary"]].\
             groupby('QID')['ADATE'].idxmin()
@@ -74,6 +74,10 @@ if __name__ == '__main__':
             groupby('QID')['ADATE'].idxmin()
         first_hosp_dict[outcome + "_secondary_first_hosp"] = ind_list
 
+    # delete columns that are no longer needed
+    for outcome in outcomes:
+        admissions = admissions.drop(columns=[outcome + "_primary", outcome + "_secondary"])
+
     for col_name in first_hosp_dict:
         # empty temporary list
         temp = pd.Series([False] * admissions_len)
@@ -81,6 +85,5 @@ if __name__ == '__main__':
         temp[first_hosp_dict[col_name]] = True
         # append to df
         admissions[col_name] = temp
-
-    admissions.to_csv("data/medpar_all/medpar_no_dask.csv", index=False)
-
+        
+    admissions.to_parquet('data/medpar_all/medpar.parquet', index=False)
